179b27616506192e4e1a44a6742ff1201b2e548d
최수연 <86272865+lotuxsoo@users.noreply.github.com>
2022-03-21 18:12:44 +0900

    Update and rename [1915]가장큰정사각형.py to [1915]가장 큰 정사각형.py

--- 1주차_dp/[1915]가장 큰 정사각형.py
+++ 1주차_dp/[1915]가장 큰 정사각형.py
@@ -7,9 +7,7 @@ dp = [[0]*m for _ in range(n)]
 
 for i in range(n):
     for j in range(m):
-        if i==0 and j==0:
-            dp[0][0] = table[0][0]
-        elif table[i][j] == 0:
+        if table[i][j] == 0:
             dp[i][j] = 0
         else:
             dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
