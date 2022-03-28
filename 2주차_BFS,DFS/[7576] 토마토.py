2ee7ee149c6792e5a75aec3c1f5a7b13dbeb3bcf
최수연 <86272865+lotuxsoo@users.noreply.github.com>
2022-03-28 22:50:57 +0900

    Update [7576] 토마토.py

--- 2주차_BFS,DFS/[7576] 토마토.py
+++ 2주차_BFS,DFS/[7576] 토마토.py
@@ -7,12 +7,6 @@ for _ in range(n):
     box.append(list(map(int, input().split()))) # 1: 익토/ 0: 안익토/ -1: x
 queue= deque([]) # 빈 리스트 추가 
 
-ans = 0 # 토마토가 모두 익는 최소 날짜
-for i in range(n):
-    for j in range(m):
-        if box[i][j]==1:
-            queue.append([i,j]) # 익토 queue에 추가 
-
 def bfs(): # 재귀x
     while queue:
         x,y = queue.popleft()
@@ -20,10 +14,18 @@ def bfs(): # 재귀x
             nx, ny = x+dx, y+dy
 
             if 0<=nx<n and 0<=ny<m and box[nx][ny]==0:
-                # 익히고 1을 더해주면서 횟수 세기
+                # 익토로 만들고 큐에 추가 
                 box[nx][ny]=box[x][y]+1
-                queue.append([nx,ny]) # 익토 queue에 추가
+                queue.append([nx,ny]) 
+
+for i in range(n):
+    for j in range(m):
+        if box[i][j]==1: # 익토 queue에 추가
+            queue.append([i,j])  
+
 bfs()
+
+ans = 0 # 토마토가 모두 익는 최소 날짜
 
 for i in box:
     for j in i:
@@ -33,4 +35,3 @@ for i in box:
     # 다 익혔으면 답은 최댓값 
     ans = max(ans, max(i))
 print(ans-1) # 처음을 1로 시작했으므로 
-            
