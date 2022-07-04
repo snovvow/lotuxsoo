4417f9982781ad8751ca862ede3fabd9c328412e
최수연 <86272865+lotuxsoo@users.noreply.github.com>
2022-07-04 22:02:49 +0900

    [BOJ] LCS / 골드5

---
+++ 9주차_문자열/[9251] LCS.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+s1 = input().rstrip()
+s2 = input().rstrip()
+
+# dp테이블 생성
+matrix = [[0]*(len(s2)+1) for _ in range(len(s1)+1)]
+
+for i in range(1, len(s1)+1):
+  for j in range(1, len(s2)+1):
+    if s1[i-1] == s2[j-1]:
+      matrix[i][j] = matrix[i-1][j-1]+1
+    else:
+      matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1])
+
+print(matrix[-1][-1])
